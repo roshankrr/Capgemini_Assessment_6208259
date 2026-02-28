9ded8d7c0c8614c2db78e114dbd358232f4dbce3
roshankrr <roshankrr16@gmail.com>
2026-02-28 14:42:57 +0530

    task 3 and 4

---
+++ Playwright/tests/task3.spec.ts
@@ -0,0 +1,14 @@
+import { test } from "@playwright/test";
+
+test("task1", async ({ page }) => {
+  await page.goto("https://demoapps.qspiders.com/ui?scenario=1");
+  await page.locator("//input[@id='name']").fill("Roshan Kumar");
+  await page.locator("//input[@id='email']").fill("roshankrr16@gmail.com");
+  await page.locator("//input[@id='password']").fill("@Abv@2405");
+  await page.locator("//button[@type='submit']").click();
+  await page.locator("//input[@id='email']").fill("roshankrr16@gmail.com");
+  await page.locator("//input[@id='password']").fill("@Abv@2305");
+  await page.locator("//button[@type='submit']").click();
+
+  await page.screenshot({ path: "./screenshot/userlogin.png" });
+});
